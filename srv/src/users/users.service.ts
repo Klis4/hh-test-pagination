b9b4981d0dca3c0ce2dac00fca7a4f4030173149
klis4 <kleshcenok.ilya@gmail.com>
2024-04-24 22:10:29 +0300

    Add pagination to getAllUsers endpoint

--- srv/src/users/users.service.ts
+++ srv/src/users/users.service.ts
@@ -3,6 +3,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UsersEntity } from './users.entity';
 import { Repository } from 'typeorm';
 
+interface PaginatedUsers {
+  page: number;
+  perPage: number;
+  totalPages: number;
+  users: UsersEntity[];
+}
 @Injectable()
 export class UserService {
   private readonly logger = new Logger(UserService.name);
@@ -13,7 +19,29 @@ export class UserService {
   ) {}
 
   // get list of all users
-  async findAll(): Promise<UsersEntity[]> {
-    return await this.usersRepo.find();
+  async findAll(page?: number, perPage?: number): Promise<PaginatedUsers> {
+    const users = await this.usersRepo.find();
+
+    if (page && perPage) {
+      const totalPages = Math.ceil(users.length / perPage);
+
+      const firstUserIndex = (page - 1) * perPage;
+      const lastUserIndex = page * perPage <= users.length - 1 ? page * perPage : users.length - 1;
+      const usersPerPage = users.slice(firstUserIndex, lastUserIndex);
+
+      return {
+        page: page,
+        perPage: perPage,
+        totalPages: totalPages,
+        users: usersPerPage,
+      };
+    } else {
+      return {
+        page: 1,
+        perPage: users.length,
+        totalPages: 1,
+        users: users,
+      };
+    }
   }
 }
